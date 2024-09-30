8656d1a739d43246030b314b21a94e74c44e5d5d
PARK_YIJUN <tnswo2445@gmail.com>
2024-09-30 12:50:00 +0900

    dsa

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-int main() {
+int Recursive(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	return (n * Recursive)
+}
 
+int main(void) {
 
-	printf("Hi\n");
-	printf("コンパイル");
+	
 
-
-
+	printf("%d\n", Min<int>(114, 150));
+	printf("%f\n", Min<float>(1.5f, 10.0f));
+	printf("%.9lf\n", Min<double>(9.544417156, 10.01215456));
+	printf("%c\n", Min('f', 'a'));
 
 	return 0;
 }
