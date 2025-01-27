6c96f2ff84b0189ed9fbbe8cbb7a7ade991412c8
PARK_YIJUN <tnswo2445@gmail.com>
2025-01-27 14:08:04 +0900

    Class Template

--- main.cpp
+++ main.cpp
@@ -2,29 +2,49 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
-#include "Enemy.h"
+#include <iostream>
+using namespace std;
 
-void LodingMessage(const char* message, int delayTime, int dots = 3) {
-	printf("\n%s", message);
-	for (int i = 0; i < dots; ++i) {
-		Sleep(delayTime);
-		printf(".");
+template <typename  T1, typename T2>
+class MinClass {
+
+private:
+
+	T1 value1;
+	T2 value2;
+
+public:
+
+	MinClass(T1 v1, T2 v2) : value1(v1), value2(v2) {}
+
+	auto Min() -> decltype(value1 < value2 ? value1 : value2) {
+		return (value1 < value2) ? value1 : value2;
 	}
-	printf("\n");
-}
 
+	void display() {
+		cout << "Comparing " << value1 << " and " << value2 << ": Min = " << Min() << endl;
+	}
+
+
+
+};
 
 int main(void) {
-	Enemy enemy;
-	printf("Initial Phase:");
-	enemy.Update();
-	enemy.SetPhase(Enemy::Phase::Shoot);
-	LodingMessage("After Changing to Phase.", 1000);
-	printf("After Phase:");
-	enemy.Update();
-	enemy.SetPhase(Enemy::Phase::Retreat);
-	LodingMessage("After Changing to Phase.", 1000);
-	printf("After Phase:");
-	enemy.Update();
+	
+	MinClass<int, int> num1(3, 5);
+	MinClass<int, float> num2(7, 6.2f);
+	MinClass<int, double> num3(8, 9.15);
+	MinClass<float, float> num4(2.3f, 2.2f);
+	MinClass<float, double> num5(3.7f, 3.2);
+	MinClass<double, double> num6(5.7, 9.1);
+
+	num1.display();
+	num2.display();
+	num3.display();
+	num4.display();
+	num5.display();
+	num6.display();
+
+	return 0;
 
 }
