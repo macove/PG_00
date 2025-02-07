2b3af76053621ed62c41dc1cff92440bbbac62d9
PARK_YIJUN <tnswo2445@gmail.com>
2025-02-07 16:52:25 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,49 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <windows.h>
 #include <iostream>
 using namespace std;
 
-template <typename  T1, typename T2>
-class MinClass {
-
-private:
-
-	T1 value1;
-	T2 value2;
-
+class value {
 public:
-
-	MinClass(T1 v1, T2 v2) : value1(v1), value2(v2) {}
-
-	auto Min() -> decltype(value1 < value2 ? value1 : value2) {
-		return (value1 < value2) ? value1 : value2;
+	virtual void Values() const {
+		printf("Something I think is valuable ");
+	}
+	virtual ~value() {};
+};
+class peopleA : public value {
+public:
+	void Values() const override {
+		printf("Money/");
 	}
 
-	void display() {
-		cout << "Comparing " << value1 << " and " << value2 << ": Min = " << Min() << endl;
+};
+class peopleB : public value {
+public:
+	void Values() const override {
+		printf("Family");
 	}
 
-
+};
+class peopleC : public value {
+public:
+	void Values() const override {
+		printf("Experience");
+	}
 
 };
 
 int main(void) {
 	
-	MinClass<int, int> num1(3, 5);
-	MinClass<int, float> num2(7, 6.2f);
-	MinClass<int, double> num3(8, 9.15);
-	MinClass<float, float> num4(2.3f, 2.2f);
-	MinClass<float, double> num5(3.7f, 3.2);
-	MinClass<double, double> num6(5.7, 9.1);
+	
+	value* peoples[3];
 
-	num1.display();
-	num2.display();
-	num3.display();
-	num4.display();
-	num5.display();
-	num6.display();
+
+	peoples[0] = new peopleA();
+	peoples[1] = new peopleB();
+	peoples[2] = new peopleC();
+
+	for (int i = 0; i < 3; i++) {
+		peoples[i]->Values();
+	}
+
+	for (int i = 0; i < 3; i++) {
+		delete peoples[i];
+	}
 
 	return 0;
 
