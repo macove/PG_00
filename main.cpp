5fa073753bda1ea9b2786e5312edcb8970db9ca4
PARK_YIJUN <tnswo2445@gmail.com>
2025-02-07 22:46:07 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,53 +1,28 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <stdio.h>
 #include <iostream>
+
+
 using namespace std;
 
-class Value {
-public:
-	virtual void GetValue() const {
-		printf("Something I think is valuable ");
-	}
-	virtual ~Value() {};
+class IShape {
+	virtual float Area() const = 0;
+	virtual void Draw() const = 0;
+	virtual ~IShape() {}
 };
-class PersonA : public Value {
+class Circle : public IShape {
 public:
-	void GetValue() const override {
-		printf("Money\n");
-	}
-
-};
-class PersonB : public Value {
-public:
-	void GetValue() const override {
-		printf("Family\n");
-	}
-
-};
-class PersonC : public Value {
-public:
-	void GetValue() const override {
-		printf("Experience\n");
-	}
+	
+private:
+	float radius;
 
 };
 
 int main(void) {
 	
 	
-	Value* people[3];
-
-
-	people[0] = new PersonA();
-	people[1] = new PersonB();
-	people[2] = new PersonC();
-
-	for (int i = 0; i < 3; i++) {
-		people[i]->GetValue();
-	}
-
-	for (int i = 0; i < 3; i++) {
-		delete people[i];
-	}
+	
 
 	return 0;
 
