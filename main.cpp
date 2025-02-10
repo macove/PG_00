40c0fb2a6e72262501bd77e9710c7554e27d8ebd
PARK_YIJUN <tnswo2445@gmail.com>
2025-02-10 12:25:07 +0900

    f

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
-int main() {
+
+template <typename T>
+T Min(T a, T b) {
+	return (a < b) ? a : b;
+}
+template <>
+char Min<char>(char a, char b) {
+	printf("数字以外は代入できません\n");
+	return '\0';
+}
+int Recursive(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	return n * Recursive(n - 1);
+}
+
+int main(void) {
 
 
-	printf("Hi\n");
-	printf("コンパイル");
+	printf("%d\n", Min<int>(114, 150));
+	printf("%f\n", Min<float>(1.5f, 10.0f));
+	printf("%.9lf\n", Min<double>(9.544417156, 10.01215456));
+	printf("%c\n", Min('f', 'a'));
 
 
 
